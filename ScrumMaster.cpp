5ff6d1f92c8126fbaeb7e242f123fb43a06d9de1
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:20:29 +0200

    Delete ScrumMaster.cpp

--- ScrumMaster.cpp
+++
@@ -1,6 +0,0 @@
-#include "ScrumMaster.h"
-
-void ScrumMaster::programMeeting(Meeting meeting, Date date, Meeting meeting, Date date) {
-	// TODO - implement ScrumMaster::programMeeting
-	throw "Not yet implemented";
-}
