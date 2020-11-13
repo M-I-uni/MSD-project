b0e22bdc544eafe967178d79127882da93efcf9e
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:22:59 +0200

    Delete SprintBacklog.java

--- SprintBacklog.java
+++
@@ -1,7 +0,0 @@
-public class SprintBacklog extends Backlog {
-	public Sprint _unnamed_Sprint_;
-
-	public Task__ fetchNoOfTasks(int aNoOfTasks) {
-		throw new UnsupportedOperationException();
-	}
-}
