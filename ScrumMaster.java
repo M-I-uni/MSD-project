2a8897bec378dcdd5e914de7eae1de220d769e2f
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:20:50 +0200

    Delete ScrumMaster.java

--- ScrumMaster.java
+++
@@ -1,8 +0,0 @@
-public class ScrumMaster extends Employee {
-	public ScrumTeam _unnamed_ScrumTeam_;
-	public Meeting _organizes;
-
-	public void programMeeting(Meeting aMeeting, Date aDate) {
-		throw new UnsupportedOperationException();
-	}
-}
