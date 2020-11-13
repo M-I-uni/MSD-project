fc042c9f97e35725c5c8c1f031d72b3cdd9706fa
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:14:06 +0200

    Delete Daily.cpp

--- Daily.cpp
+++
@@ -1,6 +0,0 @@
-#include "Daily.h"
-
-void Daily::performTaskUpdates(Task taskList[], State isPerformed, Task taskList[], State isPerformed) {
-	// TODO - implement Daily::performTaskUpdates
-	throw "Not yet implemented";
-}
