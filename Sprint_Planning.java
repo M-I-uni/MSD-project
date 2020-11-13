604e6d8273cd489284823fdfc16678e89b4285d6
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:23:11 +0200

    Delete Sprint_Planning.java

--- Sprint_Planning.java
+++
@@ -1,9 +0,0 @@
-public class Sprint_Planning extends Meeting {
-
-	/**
-	 * Team commits what can be delivered until end of Sprint
-	 */
-	public void performSprintCommit(int aNoOfTasks) {
-		throw new UnsupportedOperationException();
-	}
-}
