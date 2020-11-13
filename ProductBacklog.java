ad98cd6adfa69a0db760246120883a43d2bc15f0
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:19:11 +0200

    Delete ProductBacklog.java

--- ProductBacklog.java
+++
@@ -1,8 +0,0 @@
-public class ProductBacklog extends Backlog {
-	public ProductOwner _unnamed_ProductOwner_;
-	public Project _unnamed_Project_;
-
-	public Task__ viewRemainingTasks() {
-		throw new UnsupportedOperationException();
-	}
-}
