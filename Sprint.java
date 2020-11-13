7e4f8ef05bf133f34a49461e4b35c408cfc4e675
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:22:24 +0200

    Delete Sprint.java

--- Sprint.java
+++
@@ -1,8 +0,0 @@
-public class Sprint {
-	private int _sprintID;
-	private Backlog _sprintBacklog;
-	private Date _start;
-	private Date _end;
-	public Project _unnamed_Project_;
-	public SprintBacklog _unnamed_SprintBacklog_;
-}
