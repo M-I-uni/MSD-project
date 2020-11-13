48cac4519c7e72bc2d56694befdfeb7526b3f428
M-I-uni <72969873+M-I-uni@users.noreply.github.com>
2020-11-13 17:16:41 +0200

    Delete Task (1).java

--- Task (1).java
+++
@@ -1,9 +0,0 @@
-public class Task {
-	private State _status;
-	private int _taskId;
-	private Developer _assignedDev;
-	private int _priority;
-	public Developer _unnamed_Developer_;
-	public State _unnamed_State_;
-	public Backlog _unnamed_Backlog_;
-}
